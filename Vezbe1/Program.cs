84a0ff24558a75aa8bfec4f0fc3b60af23387302
vanredni9 <vanredni9@ITCRS.edu>
2024-03-07 11:47:07 +0100

    Add project files.

---
+++ Vezbe1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vezbe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* string ime;
+             string prezime;
+             string grad;
+
+             //Ispis korisniku
+             Console.WriteLine("Unesite ime:");
+
+             //Iscitavanje sa konzole
+             ime = Console.ReadLine();
+
+
+             Console.WriteLine("Unesite prezime:");
+             prezime = Console.ReadLine();
+
+             Console.WriteLine("Unesite grad:");
+             grad = Console.ReadLine();
+
+             Console.WriteLine("Uneli ste osobu:");
+             Console.WriteLine(ime + " " + prezime + " " + grad);
+
+             Console.ReadKey();*/
+
+           /* int prvi, drugi, rezultat;
+            string prviBroj, drugiBroj;
+
+            Console.WriteLine("Unesite prvi broj:");
+
+            prviBroj = Console.ReadLine();
+
+            Console.WriteLine("Unesite drugi broj:");
+
+            drugiBroj = Console.ReadLine();
+
+            prvi = int.Parse(prviBroj);
+            drugi = int.Parse(drugiBroj);
+
+            rezultat = prvi + drugi;
+            Console.WriteLine("REZULTAT SABIRANJA JE:");
+            Console.WriteLine(prvi+"+"+drugi+"="+rezultat);
+            Console.ReadKey();*/
+
+
+        }
+    }
+}
